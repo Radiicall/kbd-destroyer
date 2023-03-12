2885eb53148f4a64039d52e5f2710942cebe56b6
Radiicall <66682497+Radiicall@users.noreply.github.com>
2023-03-12 01:52:46 +0100

    :)

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,68 @@
-use std::io::{Write, BufWriter};
 use clap::Parser;
+use std::io::{BufWriter, Write};
 
 #[derive(Parser, Debug)]
 struct Args {
     #[arg(short = 'b', long = "backlight", help = "Backlight name")]
     backlight: Option<String>,
-    #[arg(short = 's', long = "sleep", help = "Sleep time between execution in micros")]
+    #[arg(
+        short = 's',
+        long = "sleep",
+        help = "Sleep time between execution in micros"
+    )]
     sleeptime: Option<u64>,
-    #[arg(short = 'S', long = "range-start", help = "Start number for backlight range")]
+    #[arg(
+        short = 'S',
+        long = "range-start",
+        help = "Start number for backlight range"
+    )]
     rstart: Option<i32>,
-    #[arg(short = 'E', long = "range-end", help = "End number for backlight range")]
+    #[arg(
+        short = 'E',
+        long = "range-end",
+        help = "End number for backlight range"
+    )]
     rend: Option<i32>,
-    #[arg(short = 'r', long = "reverse", help = "If the range should go in reverse too")]
-    reverse: Option<bool>
+    #[arg(
+        short = 'r',
+        long = "reverse",
+        help = "If the range should go in reverse too"
+    )]
+    reverse: Option<bool>,
 }
 
 fn main() {
     let args = Args::parse();
     let time = std::time::Duration::from_micros(args.sleeptime.unwrap_or_else(|| 5000));
-    let path = format!("/sys/class/leds/{}/brightness", args.backlight.unwrap_or_else(|| "kbd_backlight".to_string()));
+    let path = format!(
+        "/sys/class/leds/{}/brightness",
+        args.backlight
+            .unwrap_or_else(|| "kbd_backlight".to_string())
+    );
     let rstart = args.rstart.unwrap_or_else(|| 0);
-    let rend = args.rend.unwrap_or_else(|| 255);
+    let rend = args.rend.unwrap_or_else(|| {
+        std::fs::read_to_string(
+            std::path::Path::new(&path)
+                .parent()
+                .unwrap()
+                .to_string_lossy()
+                .to_string()
+                + "max_brightness",
+        )
+        .unwrap()
+        .parse::<i32>()
+        .unwrap()
+    });
     loop {
         for i in rstart..=rend {
             let file = std::fs::File::create(&path).unwrap();
             let mut writer = BufWriter::new(file);
-            
+
             println!("{}", &i);
             writer.write_all(i.to_string().as_bytes()).ok();
 
             writer.flush().ok();
-            
+
             std::thread::sleep(time);
         }
         if args.reverse.unwrap_or_else(|| true) {
@@ -39,9 +71,9 @@ fn main() {
                 let mut writer = BufWriter::new(file);
                 println!("{}", &i.abs());
                 writer.write_all((i.abs()).to_string().as_bytes()).ok();
-    
+
                 writer.flush().ok();
-                
+
                 std::thread::sleep(time);
             }
         }
